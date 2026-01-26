f6197a2783a385153febdfc5742aab10ff191e03
Nikolai Levchenko <168841147+nilevforce@users.noreply.github.com>
2026-01-26 23:19:40 +0300

    Module6-task1 (#4)
    * Добавляет верстку хэдера на главной и стили к нему
    
    * Добавляет стилизацию для шапки и hero блока на главной
    
    * Добавляет стилизацию раздела "Наши услуги" на главной
    
    * Добавляет стилизацию для главной страницы
    
    * Добавляет верстку и стили для главной страницы
    
    * Обновляет стили шапки. Теперь цвет текста и границы активной ссылки пишется через page-index
    
    * Обновляет структуру классов футера на главной странице
    
    * Добавляет верстку секции каталога (список товаров)
    
    * Добавляет ссылку на страницу разработчика по ТЗ
    
    * Добавляет верстку и стилизацию секций "Дополнительные товары" и "Акции и новинки"
    
    * Добавляет верстку каталога раздела контакты и футера
    
    * Добавляет для товаров в изображение и заголовок и для кнопки заказать ссылку на товар
    
    * Добавляет для дополнительных товаров в заголовок и для кнопки заказать ссылку на товар
    
    * Добавляет верстку для остальных секций. Добавляет корректировку стилей под пиксель перфект и устойчивость к переполнению
    
    * Добавляет корректировки по линту

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+// Управление кликом на бургер меню
+const navMain = document.querySelector('.header__menu');
+const navToggle = document.querySelector('.header__button-burger');
+
+navMain.classList.remove('header__menu--nojs');
+
+navToggle.addEventListener('click', () => {
+  navMain.classList.toggle('is-open');
+  navToggle.classList.toggle('is-open');
+});
